fa4319ad460c6f9f3d8852d06946b283e4c56051
John Morley <john.morley@bjss.com>
2019-01-26 23:33:59 +0000

    Use npm start script for integration test lifecycle

--- spec/helpers/integration-test-lifecycle.ts
+++ spec/helpers/integration-test-lifecycle.ts
@@ -3,26 +3,22 @@ import { spawn, ChildProcess } from 'child_process';
 let slsOfflineProcess: ChildProcess;
 
 export const startSlsOffline = (done: any) => {
-  const dynamoInstallProcess = spawn('npx', ['sls', 'dynamodb', 'install']);
-  dynamoInstallProcess.stdout.pipe(process.stdout);
-  dynamoInstallProcess.on('exit', () => {
-    // Spawn sls as detached so it leads the process group and kills DynamoDB when it gets SIGINT
-    slsOfflineProcess = spawn('npx', ['sls', 'offline', 'start'], { detached: true });
-    slsOfflineProcess.stdout.pipe(process.stdout);
+  // Spawn sls as detached so it leads the process group and kills DynamoDB when it gets SIGINT
+  slsOfflineProcess = spawn('npm', ['start'], { detached: true });
+  slsOfflineProcess.stdout.pipe(process.stdout);
 
-    console.log(`Serverless: Offline started with PID : ${slsOfflineProcess.pid}`);
+  console.log(`Serverless: Offline started with PID : ${slsOfflineProcess.pid}`);
 
-    slsOfflineProcess.stdout.on('data', (data) => {
-      if (data.includes('Offline listening on')) {
-        console.log(data.toString().trim());
-        done();
-      }
-    });
+  slsOfflineProcess.stdout.on('data', (data) => {
+    if (data.includes('Offline listening on')) {
+      console.log(data.toString().trim());
+      done();
+    }
+  });
 
-    slsOfflineProcess.stderr.on('data', (errData) => {
-      console.log(`Error starting Serverless Offline:\n${errData}`);
-      done(errData);
-    });
+  slsOfflineProcess.stderr.on('data', (errData) => {
+    console.log(`Error starting Serverless Offline:\n${errData}`);
+    done(errData);
   });
 };
 
