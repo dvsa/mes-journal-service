f59797f6a7bf8cd2b1718d00f0966c80aa2fdda9
RLCorp <rhys.lewis@dvsa.gov.uk>
2026-01-08 10:57:09 +0000

    Simplified format-test-slots.ts

--- src/functions/searchBooking/application/helpers/format-test-slots.ts
+++ src/functions/searchBooking/application/helpers/format-test-slots.ts
@@ -1,41 +1,19 @@
-import {get} from 'lodash';
-import {ApplicationReference} from '@dvsa/mes-test-schema/categories/common';
 import {formatApplicationReference} from '@dvsa/mes-microservice-common/domain/tars';
 import {TestSlot} from '@dvsa/mes-journal-schema';
 
-export const formatTestSlots = (testSlots: TestSlot[] = [], parameterAppRef: string) => {
-  return testSlots.map((testSlot) => {
-    if (get(testSlot, 'booking.application', null)) {
-      const application = get(testSlot, 'booking.application', null);
+export const formatTestSlots = (testSlots: TestSlot[] = [], parameterAppRef: string) =>
+  testSlots.filter((slot) => {
+    const application = slot?.booking?.application;
+    if (!application) return false;
 
-      let currentAppRef: string | ApplicationReference | undefined = '';
+    // Use the DSP bookingReference field if it exists, otherwise format the TARS data into a string
+    const formattedSlotAppRef =
+          application.bookingReference ||
+          formatApplicationReference({
+            applicationId: application.applicationId || 0,
+            checkDigit: application.checkDigit || 0,
+            bookingSequence: application.bookingSequence || 0,
+          }).toString();
 
-      // If the application has a bookingReference, use that as the test is from DSP
-      if (get(application, 'bookingReference')) {
-        currentAppRef = application?.bookingReference;
-      } else {
-        // Otherwise, set up the application reference object for TARS formatting
-        currentAppRef = {
-          applicationId: application?.applicationId || 0,
-          checkDigit: application?.checkDigit || 0,
-          bookingSequence: application?.bookingSequence || 0,
-        };
-      }
-
-      // Transform the application reference into the correct format if needed.
-      let formattedSlotAppRef: string =  '';
-
-      switch (typeof currentAppRef) {
-      case 'string':
-        formattedSlotAppRef = currentAppRef;
-        break;
-      case 'object':
-        formattedSlotAppRef = formatApplicationReference(currentAppRef).toString();
-      }
-
-      if (parameterAppRef === formattedSlotAppRef) {
-        return testSlot;
-      }
-    }
-  }).filter(testSlot => testSlot);
-};
+    return formattedSlotAppRef === parameterAppRef;
+  });
